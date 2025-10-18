ae3f90d310381029502fe435f0f14859e680105c
hectorforge <hector.hernandez.dev@gmail.com>
2025-10-17 19:15:27 -0500

    Mejoras ya ingresa a spring boot

--- app/src/main/java/com/app/hotelsys/models/Usuario.kt
+++ app/src/main/java/com/app/hotelsys/models/Usuario.kt
@@ -6,5 +6,13 @@ data class Usuario(
     val apellido: String = "",
     val fechaNacimiento: String = "",
     val email: String = "",
-    val fechaRegistro: String = ""
+    val fechaRegistro: String = "",
+
+    val nombreCompleto : String = "$nombre $apellido",
+    val telefono : String = "",
+    val dni : String = "",
+
+    val numeroDocumento : String = dni,
+    val tipoDocumentoId : Int = 1,
+    val activo : Boolean = true
 )
--- app/src/main/java/com/app/hotelsys/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/app/hotelsys/ui/auth/RegisterFragment.kt
@@ -3,21 +3,27 @@ package com.app.hotelsys.ui.auth
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.hotelsys.R
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.app.hotelsys.MainActivity
 import com.app.hotelsys.databinding.FragmentRegisterBinding
+import com.app.hotelsys.models.ClienteRequest
 import com.app.hotelsys.models.Usuario
+import com.app.hotelsys.retrofit.RetrofitInstance
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.userProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.log
 
 class RegisterFragment : Fragment() {
 
@@ -62,10 +68,23 @@ class RegisterFragment : Fragment() {
             val correo = binding.editTextCorreo.text.toString().trim()
             val password = binding.editTextPassword.text.toString().trim()
 
+            val telefono = binding.editTextTelefono.text.toString().trim()
+            val dni = binding.editTextDni.text.toString().trim()
+
             if (nombre.isEmpty() || apellido.isEmpty() || fechaNacimiento.isEmpty() ||
-                correo.isEmpty() || password.isEmpty()
+                correo.isEmpty() || password.isEmpty() || telefono.isEmpty() || dni.isEmpty()
             ) {
                 Toast.makeText(requireContext(), "Completa todos los campos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (!dni.matches(Regex("^\\d{8}$"))) {
+                Toast.makeText(requireContext(), "El DNI debe tener exactamente 8 dígitos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (!telefono.matches(Regex("^9\\d{8}$"))) {
+                Toast.makeText(requireContext(), "El número de teléfono debe tener 9 dígitos y empezar con 9", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
@@ -79,7 +98,9 @@ class RegisterFragment : Fragment() {
                         apellido = apellido,
                         fechaNacimiento = fechaNacimiento,
                         email = correo,
-                        fechaRegistro = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+                        telefono = telefono,
+                        dni = dni,
+                        fechaRegistro = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date())
                     )
 
                     val profileUpdates = userProfileChangeRequest {
@@ -91,9 +112,36 @@ class RegisterFragment : Fragment() {
                         .document(user.uid)
                         .set(usuario)
                         .addOnSuccessListener {
-                            Toast.makeText(requireContext(), "Registro exitoso", Toast.LENGTH_SHORT).show()
-                            startActivity(Intent(requireContext(), MainActivity::class.java))
-                            requireActivity().finish()
+
+                            lifecycleScope.launch {
+                                try {
+                                    val request = ClienteRequest(
+                                        nombreCompleto = "${usuario.nombre} ${usuario.apellido}",
+                                        numeroDocumento = usuario.dni,
+                                        email = usuario.email,
+                                        telefono = usuario.telefono,
+                                        activo = true,
+                                        tipoDocumentoId = 1
+                                    )
+
+                                    val response = RetrofitInstance.api.createCliente(request)
+
+                                    if (response.isSuccessful) {
+                                        Toast.makeText(requireContext(), "Cliente registrado en servidor", Toast.LENGTH_SHORT).show()
+                                    } else {
+                                        Toast.makeText(requireContext(), "Error al registrar en servidor: ${response.code()}", Toast.LENGTH_SHORT).show()
+                                    }
+
+                                    Toast.makeText(requireContext(), "Registro exitoso", Toast.LENGTH_SHORT).show()
+                                    startActivity(Intent(requireContext(), MainActivity::class.java))
+                                    requireActivity().finish()
+
+                                } catch (e: Exception) {
+                                    Log.i("TEST_CONEXION", "Error: ${e.localizedMessage}")
+                                    Toast.makeText(requireContext(), "Error conexión API: ${e.localizedMessage}", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+
                         }
                         .addOnFailureListener { e ->
                             Toast.makeText(requireContext(), "Error al guardar: ${e.localizedMessage}", Toast.LENGTH_SHORT).show()
